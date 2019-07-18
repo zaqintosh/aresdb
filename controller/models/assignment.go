79bc605ec48d4a3823cbcaa7de08c1fea2afbdd0
zaqintosh <zaqintosh@gmail.com>
2019-07-18 15:44:20 -0700

    update a file
    test

--- controller/models/assignment.go
+++ controller/models/assignment.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 package models
-
+// test
 // IngestionAssignment models job assignments for a subscriber
 type IngestionAssignment struct {
 	Subscriber string              `json:"subscriber"`
